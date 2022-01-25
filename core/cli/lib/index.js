05706b364dfc723aa9818d1fe612f048ac6f6056
YunLong Zhou <yunlong.zhou@thoughtworks.com>
2022-01-25 21:13:38 +0800

    feature: check node version and downgrade root

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -6,12 +6,32 @@ module.exports = core;
 // .js file output -> module.exports|exports
 // .json file -> JSON.parse
 // any file -> .js
+const semver = require('semver')
+const colors = require('colors/safe')
 const pkg = require('../package.json')
-
+const constants = require('./const')
 const log = require('@hard-to-build/cli-log')
 
 function core() {
-    checkVersion()
+    try {
+        checkVersion()
+        checkNodeVersion()
+        checkRoot()
+    } catch (e) {
+        log.error(e.message)
+    }
+}
+
+function checkRoot() {
+    require('root-check')()
+ }
+
+function checkNodeVersion() {
+    const currentVersion = process.version
+    const lowestVersion = constants.LOWEST_NODE_VERSION
+    if (!semver.gte(currentVersion, lowestVersion)) {
+        throw new Error(colors.red(`long-cli need at least ${lowestVersion} node version`))
+    }
 }
 
 function checkVersion() {
--- utils/log/lib/index.js
+++ utils/log/lib/index.js
@@ -5,6 +5,6 @@ const log = require('npmlog')
 log.level = process.env.LOG_LEVEL ? process.env.LOG_LEVEL : 'info'
 
 log.heading = '👉'
-log.addLevel('success', { fg: 'green' })
+log.addLevel('success', 2000, { fg: 'green' })
 
 module.exports = log;
